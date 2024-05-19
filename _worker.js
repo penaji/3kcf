f028b444ec6f8328d5af3cf8449c185a3c080317
penaji <140709525+penaji@users.noreply.github.com>
2024-05-19 12:17:29 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '89d4f8f2-1e50-47d2-add0-606b26264363';
 
-//const พร็อกซีไอพีs = ['yxfd.penaji.dedyn.io'];
+const พร็อกซีไอพีs = ['yxfd.penaji.dedyn.io'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
